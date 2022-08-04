bde3910b6277ce2c28ac14f85d53de687d46facf
Valeria Mikshina <lera-mikshina@mail.ru>
2022-08-05 01:51:24 +0300

    added after review PR13

--- app.js
+++ app.js
@@ -22,4 +22,8 @@ app.use((req, res, next) => {
 app.use('/', usersRoutes);
 app.use('/', cardsRoutes);
 
+app.use((req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
 app.listen(PORT);
